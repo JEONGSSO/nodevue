81465d7fec112cd391dc1a54e6664efd1529da00
jeongsso <rpsha33@naver.com>
2018-10-26 07:59:00 +0900

    vue

--- vue/hello/src/App.vue
+++ vue/hello/src/App.vue
@@ -1,12 +1,41 @@
 <template>
-  <div id="app">tttd
+  <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <h1>{{ttt}} :: <span v-if="isVisible"> {{visit}} </span> </h1>
+      <input type="text" v-model="ttt" @keyup="visit++">   <!-- 위에 ttt 연동 -->
+      <button @click="visitCount()">visitCount</button>
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
+      <HelloWorld msg="welcom"/>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name : 'app',
+  components : {
+    HelloWorld
+  },
+  data() {  //컴포넌트는 제이슨 X 함수로 
+      return {
+        ttt : '홍길동',
+        visit : 0,
+        isVisible : false
+      }
+  },
+  methods: {
+     visitCount() {
+        this.visit ++;  //this 내 전역변수 visit를 찾음
+        this.isVisible = true;
+     }
+  }
+}
+</script>
+
 
 <style>
 #app {
